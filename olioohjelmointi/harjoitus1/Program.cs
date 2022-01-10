c638e11d5371e3d703e58982742dfba1142bb190
Erond Dumani <erond.dumani@student.careeria.fi>
2022-01-10 13:44:16 +0200

    harjoitus1

---
+++ olioohjelmointi/harjoitus1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            auto auto = new auto();
+            auto mopo = new auto();
+            auto vene = new auto();
+
+            auto.nimi = "bmw";
+            auto.nopeus = 200;
+            auto.renkaat = 4;
+
+            mopo.nimi = "mopo";
+            mopo.nopeus = 140;
+            mopo.renkaat = 2;
+
+            vene.nimi = "buster";
+            vene.nopeus = 80;
+            vene.renkaat = 2;
+
+            auto.tulostus();
+            mopo.tulostus();
+            vene.tulostus();
+
+           
+
+            
+        }
+    }
+}
---
+++ olioohjelmointi/harjoitus1/auto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace harjoitus1
+{
+    class auto
+    {
+        public string nimi;
+        public int nopeus;
+        public int renkaat;
+
+        public void tulostus()
+        {
+            Console.WriteLine("ajoneuvo " + nimi + " nopeus " + nopeus + " renkaat "+renkaat);
+        }
+    }
+}
