ffa9925ab982cecdb3b9dfe09baac255e0c2c0ab
Erond Dumani <erond.dumani@student.careeria.fi>
2022-01-10 12:58:55 +0200

    Koira esimerkki

--- olioohjelmointi/TestiApp/Program.cs
+++ olioohjelmointi/TestiApp/Program.cs
@@ -6,7 +6,22 @@ namespace TestiApp
     {
         static void Main(string[] args)
         {
+            koira koira1 = new koira();// ensimmäinen koira olio
+            koira1.nimi = "Rekku";
+            koira1.Ikä = 4;
+            koira1.rotu = "saksanpaimenkoira";
             
+            koira1.hauku();
+            koira1.TulostaData();
+
+            koira koira2= new koira();
+            koira2.nimi = "asd";
+            koira2.Ikä = 3;
+            koira2.rotu = "mopsi";
+
+            koira2.TulostaData();
+
+           
         }
     }
 }
---
+++ olioohjelmointi/TestiApp/koira.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestiApp
+{
+    class koira
+    {
+        public string nimi;
+        public int Ikä;
+        public string rotu;
+
+        public void TulostaData()
+        {
+            Console.WriteLine("koiran nimi: " + nimi +  ".Ikä:" + Ikä+ " " + "rotu " + rotu);
+            
+            
+        }
+
+        public void hauku()
+        {
+            Console.WriteLine("woof woof");
+        }
+    }
+}
